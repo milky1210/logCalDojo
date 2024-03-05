ad52b0d8826661ea4273f332923bc2ee3ccabe22
milky1210 <mitukifukumoto@gmail.com>
2024-03-05 23:59:44 +0900

    掛け算割り算の追加

---
+++ easy/times_div.py
@@ -0,0 +1,26 @@
+import json
+import random
+import math
+# 問題を生成してJSONファイルに保存
+quizzes = []
+
+for _ in range(30000): # 100問生成しますが、この数は変更可能です
+    p1, p2 = random.random(), random.random()
+    answer = p1 * 9
+    a = p2 * 9
+    if (answer > a + 2.3):
+        question = f"{int(10**a)}×{int(10**(answer-a))}=？"
+        answer = math.log10(int(10**a) * int(10**(answer-a)))
+    elif (a > answer + 2.3):
+        question = f"{int(10**a)}÷{int(10**(a-answer))}=？"
+        answer = math.log10(int(10**a)/int(10**(a-answer)))
+    else:
+        continue
+    quizzes.append({"question": question, "answer": answer})
+
+# quizzes.jsonとして保存
+with open('tmp.json', 'w') as file:
+    json.dump(quizzes, file, indent=4, ensure_ascii=False)
+
+print("quizzes.jsonを保存しました。")
+
