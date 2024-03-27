b664d5b759f0faba1b1b820fb64bb39ef031f480
milky1210 <mitukifukumoto@gmail.com>
2024-03-27 20:14:57 +0900

    areaの解説追加

--- area/area_knowledge2.py
+++ area/area_knowledge2.py
@@ -1,6 +1,33 @@
 import json
 import math
+def log_to_prefixed_string(val):
+    # 大きな値の接頭語
+    prefixes_big = {
+        6: 'k',
+        0: ''
+    }
+    
+    # 小さな値の接頭語
+    prefixes_small = {
+        -4: 'c',
+        -6: 'm',
+        -12: 'μ',
+        -18: 'n',
+        -24: 'p',
+        -30: 'f',
+        -36: 'a',
+        -42: 'z',
+        -48: 'y'
+    }
 
+    for exp, prefix in prefixes_big.items():
+        if val >= exp:
+            return f"{format(10**(val - exp), '.3g')}{prefix}"
+    
+    for exp, prefix in prefixes_small.items():
+        if val < 0 and val >= exp:
+            return f"{format(10**(val - exp), '.3g')}{prefix}"
+    return f"{format(10**(val - exp), '.3g')}"
 file_path = 'set2.txt'
 
 # ファイルを開いて内容を読み込む
@@ -17,13 +44,14 @@ for line in lines:
         try:
             # 面積を常用対数に変換
             area_log = math.log10(float(parts[-1])) + 6.0
+            
         except ValueError:
             # 面積が数値でない場合はスキップ
             continue
-        quiz_list.append({"question": f"{name}は何m^2ですか?", "answer": area_log})
+        quiz_list.append({"question": f"{name}は何m^2ですか?", "answer": area_log, "explanation": f"{log_to_prefixed_string(area_log)}m^2なので、{area_log:.2f}となります。"})
 
 
 # 辞書のリストをJSONファイルに保存
-output_path = "area_easy.json"
+output_path = "area_knowledge2.json"
 with open(output_path, 'w', encoding='utf-8') as outfile:
     json.dump(quiz_list, outfile, ensure_ascii=False, indent=4)
