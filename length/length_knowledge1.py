32a175b50ffa6b54a8a7ec9aca3b38b072b009a4
milky1210 <mitukifukumoto@gmail.com>
2024-03-28 21:06:07 +0900

    解説追加

--- length/length_knowledge1.py
+++ length/length_knowledge1.py
@@ -1,5 +1,38 @@
 import json
 import math
+log_dict ={1:0, 2:0.3, 3:0.4771, 4:0.6, 5:0.7, 6:0.777, 7:0.845, 8:0.9, 9:0.955,10:1, 20: 1.3, 30:1.4771}
+def log_to_prefixed_string(val, mode=0):
+    if(mode==0):
+        # 大きな値の接頭語
+        prefixes_big = {
+            20: '垓',
+            16: '京',
+            12: '兆',
+            8: '億',
+            4: '万',
+            0: ''
+        }
+    else:
+        # 大きな値の接頭語
+        prefixes_big = {
+            19: '京k',
+            15: '兆k',
+            11: '億k',
+            7: '万k',
+            3: 'k',
+            0: ''
+        }
+    
+
+    for exp, prefix in prefixes_big.items():
+        if val >= exp:
+            number = 10**(val - exp)
+            if number<10:
+                return f"{number:.2g}{prefix}"
+            else:
+                return f"{int(number)}{prefix}"
+    return f"{int(10**(val - exp))}"
+
 
 dict = {
     "Å": 1e-10,
@@ -22,17 +55,21 @@ quizzes = []
 for key in dict.keys():
     if dict[key]>1:
         quiz = {"question": f"1{key}は何mですか？",
-            "answer": math.log10(dict[key])}
+            "answer": math.log10(dict[key]),
+            "explanation": f"1{key}は{log_to_prefixed_string(math.log10(dict[key]),mode=1)}mです。"}
     else:
         quiz = {"question": f"1mは何{key}ですか？",
-            "answer": -math.log10(dict[key])}
+            "answer": -math.log10(dict[key]),
+            "explanation": f"1mは{log_to_prefixed_string(-math.log10(dict[key]),mode=0)}{key}"}
+            
     quizzes.append(quiz)
 
 # tan系を暗記
 for theta in [1,2,3,5,10,20,30]:
     quiz = {
         "question": f"tan{theta}は何分の一ですか？",
-            "answer": math.log10(1/math.tan(math.radians(theta)))
+        "answer": math.log10(1/math.tan(math.radians(theta))),
+        "explanation": f"tan{theta}は{1/math.tan(math.radians(theta))}です。¥n180/{theta}*πで近似できるので、2.25-{log_dict[theta]}+0.5で大まかに推論できます。"
     }
     quizzes.append(quiz)
 
